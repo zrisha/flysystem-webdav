4a81b3e5b9d1d3b1623cf5d035b7826a5608c0c6
Zak Risha <zjrisha@gmail.com>
2019-03-26 13:11:37 -0400

    namespace for client

---
+++ src/Flysystem/Client/Client.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\flysystem_webdav\Flysystem\Client;
+
+use Sabre\DAV\Client as DAVClient;
+
+class Client extends DAVClient{
+  /**
+   * Coppied from the DAV lib with small edit due to XML error
+   */
+  function propFind($url, array $properties, $depth = 0) {
+
+    $dom = new \DOMDocument('1.0', 'UTF-8');
+    $dom->formatOutput = true;
+    $root = $dom->createElementNS('DAV:', 'd:propfind');
+    $prop = $dom->createElement('d:prop');
+
+    foreach ($properties as $property) {
+
+        list(
+            $namespace,
+            $elementName
+        ) = \Sabre\Xml\Service::parseClarkNotation($property);
+
+        if ($namespace === 'DAV:') {
+            $element = $dom->createElement('d:' . $elementName);
+        } else {
+            $element = $dom->createElementNS($namespace, 'x:' . $elementName);
+        }
+
+        $prop->appendChild($element);
+    }
+
+    $dom->appendChild($root)->appendChild($prop);
+    $body = $dom->saveXML();
+
+    $url = $this->getAbsoluteUrl($url);
+
+    $request = new HTTP\Request('PROPFIND', $url, [
+        'Depth'        => $depth,
+        'Content-Type' => 'application/xml'
+    ], $body);
+
+    $response = $this->send($request);
+
+    if ((int)$response->getStatus() >= 400) {
+        throw new HTTP\ClientHttpException($response);
+    }
+
+    //EDIT: Removes any leading white space
+    $resBody = $response->getBodyAsString();
+    $resBody = strstr($resBody, '<?xml');
+    $result = $this->parseMultiStatus($resBody);
+
+    // If depth was 0, we only return the top item
+    if ($depth === 0) {
+        reset($result);
+        $result = current($result);
+        return isset($result[200]) ? $result[200] : [];
+    }
+
+    $newResult = [];
+    foreach ($result as $href => $statusList) {
+
+        $newResult[$href] = isset($statusList[200]) ? $statusList[200] : [];
+
+    }
+    return $newResult;
+  }
+}
--- src/Flysystem/Webdav.php
+++ src/Flysystem/Webdav.php
@@ -7,81 +7,16 @@
 
 namespace Drupal\flysystem_webdav\Flysystem;
 
-use Sabre\DAV\Client as DAVClient;
+use Drupal\flysystem_webdav\Flysystem\Client\Client;
 use Sabre\HTTP;
 use Drupal\Core\Logger\RfcLogLevel;
+use Drupal\flysystem_webdav\Flysystem\Adapter\WebDAVAdapter;
 use Drupal\flysystem\Flysystem\Adapter\MissingAdapter;
 use Drupal\flysystem\Plugin\FlysystemPluginInterface;
 use Drupal\flysystem\Plugin\FlysystemUrlTrait;
 use Drupal\flysystem\Plugin\ImageStyleGenerationTrait;
 use GuzzleHttp\Psr7\Uri;
 use League\Flysystem\Filesystem;
-
-
-class Client extends DAVClient{
-  /**
-   * Coppied from the DAV lib with small edit due to XML error
-   */
-  function propFind($url, array $properties, $depth = 0) {
-
-    $dom = new \DOMDocument('1.0', 'UTF-8');
-    $dom->formatOutput = true;
-    $root = $dom->createElementNS('DAV:', 'd:propfind');
-    $prop = $dom->createElement('d:prop');
-
-    foreach ($properties as $property) {
-
-        list(
-            $namespace,
-            $elementName
-        ) = \Sabre\Xml\Service::parseClarkNotation($property);
-
-        if ($namespace === 'DAV:') {
-            $element = $dom->createElement('d:' . $elementName);
-        } else {
-            $element = $dom->createElementNS($namespace, 'x:' . $elementName);
-        }
-
-        $prop->appendChild($element);
-    }
-
-    $dom->appendChild($root)->appendChild($prop);
-    $body = $dom->saveXML();
-
-    $url = $this->getAbsoluteUrl($url);
-
-    $request = new HTTP\Request('PROPFIND', $url, [
-        'Depth'        => $depth,
-        'Content-Type' => 'application/xml'
-    ], $body);
-
-    $response = $this->send($request);
-
-    if ((int)$response->getStatus() >= 400) {
-        throw new HTTP\ClientHttpException($response);
-    }
-
-    //EDIT: Removes any leading white space
-    $resBody = $response->getBodyAsString();
-    $resBody = strstr($resBody, '<?xml');
-    $result = $this->parseMultiStatus($resBody);
-
-    // If depth was 0, we only return the top item
-    if ($depth === 0) {
-        reset($result);
-        $result = current($result);
-        return isset($result[200]) ? $result[200] : [];
-    }
-
-    $newResult = [];
-    foreach ($result as $href => $statusList) {
-
-        $newResult[$href] = isset($statusList[200]) ? $statusList[200] : [];
-
-    }
-    return $newResult;
-  }
-}
 
 
 /**
